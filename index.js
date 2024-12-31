4dc3c94db3a059ab5d8f99c08541244f63a3082d
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2024-12-31 18:26:21 +0530

    updating generation params

--- index.js
+++ index.js
@@ -27,8 +27,9 @@ async function initializeWebLLMEngine() {
     document.getElementById("download-status").classList.remove("hidden");
     let selectedModel = "Llama-3.2-1B-Instruct-q4f16_1-MLC";
     const config = {
-        temperature: 0.1,
+        temperature: 0.2,
         top_p: 0.9,
+        repetition_penalty: 1.2
     };
     await engine.reload(selectedModel, config);
 }
